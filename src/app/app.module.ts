e383ef75edd018e8fa3eee81d9101653143932cd
Ahmed Tarek <a.tarek.elsayed@gmail.com>
2019-04-10 14:23:35 +0200

    Update dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,19 @@ import { ProductCardComponent } from './components/product-card/product-card.com
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
-import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
-import { FooterComponent } from './layout/footer/footer.component';
+import { NavBarComponent } from './components/layout/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 import{NgxPaginationModule} from 'ngx-pagination';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { SmallCartViewerComponent } from './components/layout/small-cart-viewer/small-cart-viewer.component';
+
+import {MatButtonModule,MatCheckboxModule,MatToolbarModule,MatInputModule,MatProgressSpinnerModule,MatCardModule,MatMenuModule, MatIconModule, MatBadgeModule} from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SmallProductViewerComponent } from './components/layout/small-product-viewer/small-product-viewer.component';
+import { CartViewerComponent } from './cart-viewer/cart-viewer.component';
+
+
 
 
 @NgModule({
@@ -22,13 +31,18 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     ProductCardComponent,
     NotFoundComponent,
     FooterComponent,
-    ProductListComponent
+    ProductListComponent,
+    SmallCartViewerComponent,
+    SmallProductViewerComponent,
+    CartViewerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgxPaginationModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    MatButtonModule, MatCheckboxModule,MatToolbarModule,MatInputModule,MatProgressSpinnerModule,MatCardModule,MatMenuModule,MatIconModule,MatBadgeModule,
+    BrowserAnimationsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
