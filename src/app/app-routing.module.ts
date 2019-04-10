23b0add854e300b399747717b86e09014f53088a
Ahmed Tarek <a.tarek.elsayed@gmail.com>
2019-04-10 23:59:06 +0200

    Update dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { CartViewerComponent } from './components/cart-viewer/cart-viewer.component';
 
 const routes: Routes = [
   {path:'',component:ProductListComponent},
   {path:'products',component:ProductListComponent},
   {path:'products/:id',component:ProductDetailsComponent},
+  {path:'cart', component:CartViewerComponent},
   {path:'**', component:NotFoundComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,17 @@ import{NgxPaginationModule} from 'ngx-pagination';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { SmallCartViewerComponent } from './components/layout/small-cart-viewer/small-cart-viewer.component';
 
-import {MatButtonModule,MatCheckboxModule,MatToolbarModule,MatInputModule,MatProgressSpinnerModule,MatCardModule,MatMenuModule, MatIconModule, MatBadgeModule} from '@angular/material';
+import {MatButtonModule,MatCheckboxModule,MatToolbarModule,MatInputModule,MatProgressSpinnerModule,MatCardModule,MatMenuModule, MatIconModule, MatBadgeModule, MatTableModule} from '@angular/material';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SmallProductViewerComponent } from './components/layout/small-product-viewer/small-product-viewer.component';
-import { CartViewerComponent } from './cart-viewer/cart-viewer.component';
+import { CartViewerComponent } from './components/cart-viewer/cart-viewer.component';
+import { ProductTitleComponent } from './components/shared/product-title/product-title.component';
+import { ProductSizeComponent } from './components/shared/product-size/product-size.component';
+import { ProductPriceComponent } from './components/shared/product-price/product-price.component';
+import { ProductDescriptionComponent } from './components/shared/product-description/product-description.component';
+import { PriceMultiplierComponent } from './components/shared/price-multiplier/price-multiplier.component';
+import { CartProductCardComponent } from './components/cart-product-card/cart-product-card.component';
 
 
 
@@ -34,14 +40,20 @@ import { CartViewerComponent } from './cart-viewer/cart-viewer.component';
     ProductListComponent,
     SmallCartViewerComponent,
     SmallProductViewerComponent,
-    CartViewerComponent
+    CartViewerComponent,
+    ProductTitleComponent,
+    ProductPriceComponent,
+    ProductSizeComponent,
+    ProductDescriptionComponent,
+    PriceMultiplierComponent,
+    CartProductCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgxPaginationModule,
     NgbModule.forRoot(),
-    MatButtonModule, MatCheckboxModule,MatToolbarModule,MatInputModule,MatProgressSpinnerModule,MatCardModule,MatMenuModule,MatIconModule,MatBadgeModule,
+    MatButtonModule, MatCheckboxModule,MatToolbarModule,MatInputModule,MatProgressSpinnerModule,MatCardModule,MatMenuModule,MatIconModule,MatBadgeModule,MatTableModule,
     BrowserAnimationsModule 
   ],
   providers: [],
