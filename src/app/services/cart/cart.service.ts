1123c7c23fe15c0f032c0d42dc29b1ecb5eeefb6
Ahmed Tarek <a.tarek.elsayed@gmail.com>
2019-04-10 14:20:23 +0200

    Extend CartService functions

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -1,44 +1,79 @@
 import { Injectable } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import { Observable } from 'rxjs';
-import { Cart } from '../../models/cart';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { CartProduct } from 'src/app/models/cart-product';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  
-  cart: Cart = { cartProducts: []}
-  products: Observable<Product>;
+
+  cart:CartProduct[] =[];
+  itemsCount:number =0;
+  totalPrice:number =0;
+
+  private cart$:BehaviorSubject<Array<CartProduct>> =  new BehaviorSubject([]);
+  private itemsCount$:BehaviorSubject<number> = new BehaviorSubject(0);
+  private totalPrice$:BehaviorSubject<number> = new BehaviorSubject(0);
 
 
-
-  constructor() {  }
+  constructor() {  
+  } 
 
 
   add(product: Product): void {
-    let cpid = this.cart.cartProducts.findIndex(p => p.product.id === product.id)
+    let cpid = this.cart.findIndex(p => p.product.id === product.id)
 
     if (cpid > -1)
-      Object.assign(this.cart.cartProducts[cpid], this.cart.cartProducts[cpid].count++);
+      Object.assign(this.cart[cpid], this.cart[cpid].count++);
 
     else
-      this.cart.cartProducts.push({ product: product, count: 1 })
+      this.cart.push({ product: product, count: 1 })
+    
+    this.updateSubsribers();
+  }
 
-    console.log('total', this.total());
+ 
+
+  remove(cartProduct: CartProduct): void {
+    let cpid = this.cart.findIndex(p => p.product.id === cartProduct.product.id);
+    this.cart.splice(cpid, 1);
+
+    this.updateSubsribers();
+  }
+
+  getCart():Observable<Array<CartProduct>>{
+    return this.cart$.asObservable();
+  }
+
+  getTotalPrice():Observable<number>{
+    return this.totalPrice$.asObservable();
+  }
+
+  getItemsCount():Observable<number>{
+    return this.itemsCount$.asObservable();
   }
 
 
-  remove(product: Product): void {
-    let cpid = this.cart.cartProducts.findIndex(p => p.product.id === product.id)
-
-    this.cart.cartProducts.splice(cpid, 1)
+  private updateSubsribers(){
+    this.cart$.next(this.cart);
+    this.itemsCount$.next(this.countItems());
+    this.totalPrice$.next(this.calculatePrice());
   }
 
-  total(sum:number = 0):number{
-     this.cart.cartProducts.forEach(el=>sum+= el.product.price * el.count)
-    return sum;
+
+  private countItems():number{
+    let count = 0;
+    this.cart.forEach(el=> count+= el.count );
+    return count;
   }
-}
+
+  private calculatePrice():number{
+    let price = 0;
+    this.cart.forEach(el=> price+= el.product.price * el.count );
+    return price;
+  }
+} 
 
